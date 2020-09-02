115898bf15e8c86a323a97117ea17554ff528802
Aliaksandr Svinarenka <asvinarenka@gmail.com>
2020-09-02 16:33:06 -0400

    conf c

--- Web/Program.cs
+++ Web/Program.cs
@@ -27,9 +27,9 @@ namespace Web
                     var env = hostingContext.HostingEnvironment;
 
                     config
-                          .Add(new WebConfigSource() { Path = "web.config", Optional = false, ReloadOnChange = true, })
+                          //.Add(new WebConfigSource() { Path = "web.config", Optional = false, ReloadOnChange = true, })
                           .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                         // .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
                           ;
 
 
@@ -63,23 +63,23 @@ namespace Web
         
     }
 
-    public class WebConfigSource : FileConfigurationSource
-    {
-        public override IConfigurationProvider Build(IConfigurationBuilder builder)
-        {
-            FileProvider = FileProvider ?? builder.GetFileProvider();
-            return new WebConfigConfigurationProvider(this);
-        }
+    //public class WebConfigSource : FileConfigurationSource
+    //{
+    //    public override IConfigurationProvider Build(IConfigurationBuilder builder)
+    //    {
+    //        FileProvider = FileProvider ?? builder.GetFileProvider();
+    //        return new WebConfigConfigurationProvider(this);
+    //    }
 
-        public class WebConfigConfigurationProvider : FileConfigurationProvider
-        {
-            public WebConfigConfigurationProvider(WebConfigSource source) : base(source) { }
+    //    public class WebConfigConfigurationProvider : FileConfigurationProvider
+    //    {
+    //        public WebConfigConfigurationProvider(WebConfigSource source) : base(source) { }
 
-            public override void Load(Stream stream)
-            {
-                Data = XDocument.Load(stream).Element("configuration").Element("appSettings")
-                    .Elements("add").ToDictionary(_ => "webconfig:" + _.Attribute("key").Value.Replace(".", string.Empty), _ => _.Attribute("value").Value);
-            }
-        }
-    }
+    //        public override void Load(Stream stream)
+    //        {
+    //            Data = XDocument.Load(stream).Element("configuration").Element("appSettings")
+    //                .Elements("add").ToDictionary(_ => "webconfig:" + _.Attribute("key").Value.Replace(".", string.Empty), _ => _.Attribute("value").Value);
+    //        }
+    //    }
+    //}
 }
