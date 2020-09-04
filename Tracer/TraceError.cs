8c86bb86d6c622887e36a8286782afc9d94e147c
Aliaksandr Svinarenka <asvinarenka@gmail.com>
2020-09-04 16:27:12 -0400

    w1

--- Tracer/TraceError.cs
+++ Tracer/TraceError.cs
@@ -14,31 +14,31 @@ namespace Tracer
             Trace.TraceError(message);
             var tracesource = new TraceSource("Appharbortracesource", defaultLevel: SourceLevels.All);
             tracesource.TraceEvent(TraceEventType.Critical, 0, message);
-            _sb.Append($"<p>{DateTime.Now.ToString()} : {message}</p>");
+            _sb.Append($"{DateTime.Now.ToString()} : {message}<br>");
         }
 
         public static void Error(string message)
         {
             Trace.TraceError(message);
-            _sb.Append($"<p>{DateTime.Now.ToString()} : {message}</p>");
+            _sb.Append($"{DateTime.Now.ToString()} : {message}<br>");
         }
 
         public static void Error(string message, string exception)
         {
             Trace.TraceError(message + " " + exception);
-            _sb.Append($"<p>{DateTime.Now.ToString()} : {message}</p>");
+            _sb.Append($"{DateTime.Now.ToString()} : {message}<br>");
         }
 
         public static void Error(Exception e, string message = "")
         {
             //Trace.TraceError("ERROR: " + e.Message + " " + e.InnerException?.Message);
             Trace.TraceError("ERROR: " + "Custom message - " + message + Environment.NewLine + e.ToString());
-            _sb.Append($"<p>{DateTime.Now.ToString()} : {message}</p>");
+            _sb.Append($"{DateTime.Now.ToString()} : {message}<br>");
         }
 
-        public static string GetLog()
+        public static string[] GetLog()
         {
-            return (_sb != null) ? _sb.ToString() : "";
+            return (_sb != null) ? _sb.ToString().Split("<br>") : new string[0];
         }
     }
 }
