abec50b363c797a71f941e928cd5f21d7662933d
Aliaksandr Svinarenka <asvinarenka@gmail.com>
2020-09-04 12:45:15 -0400

    bot initial init should be upon app start

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -1,8 +1,10 @@
 using System.Threading;
 using DatabaseInteractions;
+using Logic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,12 @@ namespace Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+                
+            //TODO: DB setup
+            //var connection = Configuration.GetConnectionString("DefaultConnection");
+            //services.AddDbContext<BotDBContext>(options => options.UseSqlServer(connection));
+            //services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +51,7 @@ namespace Web
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                Bot.Get();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
