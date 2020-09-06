3cb21dfdf9dffc1aec18baaf127da2bdcb33c706
Aliaksandr Svinarenka <asvinarenka@gmail.com>
2020-09-06 14:29:38 -0400

    mapped controllers

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -62,9 +62,10 @@ namespace Web
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
+                //endpoints.MapControllerRoute(
+                //    name: "default",
+                //    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
