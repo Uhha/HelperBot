46252025c7e62d0e591a5a3169cfcd5ad31ab4dd
Aliaksandr Svinarenka <asvinarenka@gmail.com>
2020-09-06 23:37:31 -0400

    SQLSERVER_CONNECTION_STRING

--- DatabaseInteractions/Config.cs
+++ DatabaseInteractions/Config.cs
@@ -32,7 +32,7 @@ namespace DatabaseInteractions
 
         public static string DBConnectionString
         {
-            get { return string.IsNullOrEmpty(_config["DBConnectionString"]) ? _webConfig["DBConnectionString"] : _config["DBConnectionString"]; }
+            get { return string.IsNullOrEmpty(_config["DBConnectionString"]) ? _webConfig["SQLSERVER_CONNECTION_STRING"] : _config["DBConnectionString"]; }
         }
 
         public static void SetConfig(IConfiguration icon) => _config = icon;
